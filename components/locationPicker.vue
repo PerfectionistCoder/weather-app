e0e615d469a56ac741c53ff3ccf4260343f62de9
3b4d80a2-0c72-4839-8000-c72d4daf561f <3b4d80a2-0c72-4839-8000-c72d4daf561f>
2024-10-21 11:08:02 +0100

    locationpicker buttons add, auto detect geolocation

--- components/locationPicker.vue
+++ components/locationPicker.vue
@@ -1,33 +1,118 @@
 <template>
   <div>
-    <LMap
-      ref="map"
-      v-model:zoom="zoom"
-      :use-global-leaflet="false"
-      :center="geo"
-    >
-      <LTileLayer
-        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        :min-zoom="1"
-        :max-zoom="20"
-      />
-      <LMarker
-        :lat-lng="geo"
-        :draggable="true"
-        @update:lat-lng="(e) => console.log(e)"
-      />
-    </LMap>
+    <div id="element" class="h-screen w-screen" />
+    <div class="center map-components bottom-2 flex gap-2">
+      <button @click="getLocation">
+        <Icon name="tabler:location" mode="svg" />
+        find your location
+      </button>
+      <button
+        class="primary"
+        :disabled="locale == undefined"
+        @click="
+          () => {
+            location = locale
+            showMap = false
+          }
+        "
+      >
+        <Icon name="tabler:check" mode="svg" />
+        confirm
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import 'leaflet/dist/leaflet.css'
-import { LMap, LTileLayer, LMarker } from '@vue-leaflet/vue-leaflet'
+import L from 'leaflet'
+import markerIcon from '~/assets/icons/marker.svg'
+import type { Geolocation } from '~/types'
 
-const zoom = ref(5)
+const location = defineModel<Geolocation>('location')
+const locale = ref<Geolocation>()
+const showMap = defineModel<boolean>('show-map')
 
-const geo = useGetGeolocation()
+const marker = ref<L.Marker | undefined>()
+const setMarker = ({
+  latlng,
+}: {
+  [key: string]: any
+  latlng: L.LatLngExpression
+}) => {
+  if (map.value && !marker.value) {
+    const size = 32
+    marker.value = L.marker(latlng, {
+      draggable: true,
+      autoPan: true,
+      icon: L.icon({
+        iconUrl: markerIcon,
+        iconSize: [size, size],
+        iconAnchor: [size / 2, size],
+      }),
+    }).addTo(map.value)
+  } else marker.value?.setLatLng(latlng)
+  locale.value = useLatLngToArray(marker.value!.getLatLng())
+}
+
+const zoom = 3
+const minZoom = 2
+const maxZoom = 17
+const focusZoom = 10
+
+const map = ref<L.Map | undefined>()
+onMounted(() => {
+  map.value = L.map('element', {
+    attributionControl: false,
+    boxZoom: false,
+    center: [0, 0],
+    inertia: true,
+
+    zoom,
+    minZoom,
+    maxZoom,
+  })
+  map.value.removeControl(map.value.zoomControl)
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png')
+    .setZIndex(0)
+    .addTo(map.value)
+
+  map.value.on('click', (event) => setMarker(event))
+
+  if (location.value) {
+    const latlng = [
+      location.value.lat,
+      location.value.lng,
+    ] as L.LatLngExpression
+    map.value.setView(location.value, focusZoom)
+    setMarker({ latlng })
+  }
+})
+
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latlng = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        }
+        map.value?.setView(latlng, focusZoom)
+        setMarker({ latlng })
+      },
+      (err) => {
+        switch (err.code) {
+          case 1:
+            alert('Permission for acquiring your geolocation is denied.')
+            break
+          default:
+            alert('Fail to acquire your geolocation.')
+            break
+        }
+      }
+    )
+  } else alert('Geolocation is not supported by this browser.')
+}
 </script>
 
 <style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,18 @@
 <template>
-  <div v-if="status == 'pending'">
-    <LoadingAnimation />
+  <div v-if="showMap">
+    <LocationPicker v-model:location="location" v-model:show-map="showMap" />
   </div>
-  <div v-else></div>
+  <div v-else>
+    {{ location }}
+    <button @click="showMap = true">show map</button>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import type { FetchResponse } from 'ofetch'
+import type { Geolocation } from '~/types'
 
-const navigate = async (
-  path: string,
-  response: FetchResponse<any> | undefined
-) => {
-  const data = response?._data
-  await navigateTo({
-    path,
-    query: {
-      latitude: data?.lat,
-      longitude: data?.lon,
-      city: data?.city,
-    },
-  })
-}
-
-const locationPickerPage = '/map'
-
-const { status } = await useFetch('json/false', {
-  params: {
-    fields: 'status,lat,lon,city',
-  },
-  baseURL: 'http://ip-api.com/',
-  server: false,
-  cache: 'force-cache',
-  lazy: true,
-
-  async onRequestError({ response }) {
-    await navigate(locationPickerPage, response)
-  },
-  async onResponse({ response }) {
-    if (response._data.status == 'fail')
-      await navigate(locationPickerPage, response)
-    else await navigate('/', response)
-  },
-  async onResponseError({ response }) {
-    await navigate(locationPickerPage, response)
-  },
-})
+const location = ref<Geolocation>()
+const showMap = ref(true)
 </script>
 
 <style></style>
